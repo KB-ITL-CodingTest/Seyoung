4c73c9b39c06d08e178e0d329530953a47d87127
syann97 <shallom75321@gmail.com>
2025-05-22 13:25:46 +0900

    Week 8: Solved DP Problems

--- [8주차] DP/개별 문제/LCS 2.java
+++ [8주차] DP/개별 문제/LCS 2.java
@@ -6,14 +6,16 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 1. 문자열 2개 입력 받고 dp 테이블 초기화
         String str1 = " " + br.readLine();
         String str2 = " " + br.readLine();
-        String answer = "";
         int N = str1.length() - 1;
         int M = str2.length() - 1;
 
         int[][] dp = new int[N+1][M+1];
 
+
+        // 2. 점화식을 바탕으로 dp 테이블 구성
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
                 if (str1.charAt(i) == str2.charAt(j)) dp[i][j] = dp[i - 1][j - 1] + 1;
@@ -24,21 +26,28 @@ public class Main {
     }
 
 
+    // 3. DP 테이블의 마지막 위치에서부터 역추적
     static void traceLCS(int y, int x, int[][] dp, String str) {
         StringBuilder sb = new StringBuilder();
+        
+        // 역추적
         while (y > 0 && x > 0) {
-            if (dp[y][x] != dp[y-1][x]) {
+            // 현재 좌표로부터 왼쪽 또는 위쪽에 자신과 같은 값이 있을 때
+            // 해당 좌표로 이동 (왼쪽 우선)
+            if (dp[y][x] == dp[y][x-1]) {
+                x--;
+            }
+            else if (dp[y][x] == dp[y-1][x]) {
+                y--;
+            }
+            
+            // 현재 좌표에서 왼쪽과 위에 자신과 같은 값이 없는 경우
+            // 현재 문자를 StringBuilder에 저장하고 좌대각으로 이동
+            else {
                 sb.append(str.charAt(y));
                 x--;
                 y--;
             }
-            else if (dp[y][x] == dp[y][x-1]) {
-                x--;
-            }
-            else {
-                y--;
-            }
-
         }
         System.out.println(sb.length());
         System.out.println(sb.reverse());
